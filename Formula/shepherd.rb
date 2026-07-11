493504dd6526d50a584ff3f7d2d27ddf0c1d4804
Jonathan Warykowski <jonathan@myzeller.com>
2026-07-11 17:25:39 +1000

    feat: bump shepherd to v0.0.2 (command API)

--- Formula/shepherd.rb
+++ Formula/shepherd.rb
@@ -1,8 +1,8 @@
 class Shepherd < Formula
   desc "Interactive todo board backed by a markdown file"
   homepage "https://github.com/jwarykowski/shepherd"
-  url "https://github.com/jwarykowski/shepherd/archive/refs/tags/v0.0.1.tar.gz"
-  sha256 "4145ef9a5eea7b6d9e3cf47b33e441e0e33a2f883d8c398e425a1324110cb4bc"
+  url "https://github.com/jwarykowski/shepherd/archive/refs/tags/v0.0.2.tar.gz"
+  sha256 "4a668b5f022eef9f5469bde7f823454959e256714de0fa64dd591a19f368d820"
   license "MIT"
   head "https://github.com/jwarykowski/shepherd.git", branch: "master"
 
