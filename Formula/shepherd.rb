f126eb366ba96037368ac01afe07ae9f732c1632
Jonathan Warykowski <jonathan@myzeller.com>
2026-07-11 14:47:12 +1000

    feat: add shepherd formula (v0.0.1)

---
+++ Formula/shepherd.rb
@@ -0,0 +1,18 @@
+class Shepherd < Formula
+  desc "Interactive todo board backed by a markdown file"
+  homepage "https://github.com/jwarykowski/shepherd"
+  url "https://github.com/jwarykowski/shepherd/archive/refs/tags/v0.0.1.tar.gz"
+  sha256 "4145ef9a5eea7b6d9e3cf47b33e441e0e33a2f883d8c398e425a1324110cb4bc"
+  license "MIT"
+  head "https://github.com/jwarykowski/shepherd.git", branch: "master"
+
+  depends_on "go" => :build
+
+  def install
+    system "go", "build", *std_go_args(ldflags: "-s -w")
+  end
+
+  test do
+    assert_match "filter", shell_output("#{bin}/shepherd --help 2>&1")
+  end
+end
