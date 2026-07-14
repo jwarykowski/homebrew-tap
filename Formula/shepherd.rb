2814755d553522bdf6bfde4310a21f3eee368dbc
Jonathan Warykowski <jonathan@myzeller.com>
2026-07-15 09:03:24 +1000

    shepherd 0.9.0

--- Formula/shepherd.rb
+++ Formula/shepherd.rb
@@ -1,8 +1,8 @@
 class Shepherd < Formula
   desc "Interactive todo board backed by a markdown file"
   homepage "https://github.com/jwarykowski/shepherd"
-  url "https://github.com/jwarykowski/shepherd/archive/refs/tags/v0.7.0.tar.gz"
-  sha256 "93fd021609b044433a06f8ff534ed06df0d5b4d333fd310e1eae6b977f93495e"
+  url "https://github.com/jwarykowski/shepherd/archive/refs/tags/v0.9.0.tar.gz"
+  sha256 "f6f929f10cbc8a5a61ea2f91dd8d86455b8496fbffcd46efd610aae554808cdb"
   license "MIT"
   head "https://github.com/jwarykowski/shepherd.git", branch: "master"
 
